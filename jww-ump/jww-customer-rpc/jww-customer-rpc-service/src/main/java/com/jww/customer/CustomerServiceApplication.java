3d082c95eaef34216413a27f85d849b3b504a82f
ronaldo_wang@163.com <ronaldo_wang@163.com>
2018-09-24 15:41:57 +0800

    1、重新安排工程目录

---
+++ jww-ump/jww-customer-rpc/jww-customer-rpc-service/src/main/java/com/jww/customer/CustomerServiceApplication.java
@@ -0,0 +1,27 @@
+package com.jww.customer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @author wanyong
+ * @description: 启动类
+ * @date 2017/11/17 00:34
+ */
+@Slf4j
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.jww.**.rpc.service.impl", "com.jww.common.db", "com.jww.common.core", "com.jww.common.mdb", "com.jww.common.redis"})
+@ImportResource(value = {"classpath:dubbo/providers.xml"})
+@MapperScan(basePackages = {"com.jww.**.dao.mapper"})
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(CustomerServiceApplication.class);
+        springApplication.run(args);
+        log.info("========== ServiceApplication启动成功 ==========");
+    }
+}
